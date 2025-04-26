c70f4634286a6ff76567c2e7bc881deb93a349ff
Dennis Ploetner <re@lloc.de>
2025-04-26 10:47:29 +0200

    Slide 25

--- docs/presentation/slide-25.example.php
+++ docs/presentation/slide-25.example.php
@@ -1,10 +1,15 @@
 <?php
 
-$date = date_i18n( get_option( 'date_format' ) );
+$date = date_i18n(
+	get_option( 'date_format' )
+);
 
 echo esc_html( $date );
 
 $timestamp = mktime( 13, 42, 23, 7, 17, 2025 );
-$date      = date_i18n( get_option( 'date_format' ), $timestamp );
+$date      = date_i18n(
+	get_option( 'date_format' ),
+	$timestamp
+);
 
 echo esc_html( $date );
