0b01d1df3de92f22ee5969f2b8e6c314fa243739
Dennis Ploetner <re@lloc.de>
2025-04-26 10:51:56 +0200

    Slide 26

--- docs/presentation/slide-26.example.php
+++ docs/presentation/slide-26.example.php
@@ -4,6 +4,9 @@ $number = number_format_i18n( 1234567.89 );
 
 echo esc_html( $number );
 
-$number = number_format_i18n( 1234567.89, 2 );
+$number = number_format_i18n(
+	1234567.89,
+	2
+);
 
 echo esc_html( $number );
